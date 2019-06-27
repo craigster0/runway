7c448c258886c4a84e0dc8d0f42961d5aa1c7656
Gerard Gine <ggine@swiftstack.com>
2019-06-27 13:53:07 -0700

    Add option to specify login user in bash_on_current_container.sh

--- bin/bash_on_current_container.sh
+++ bin/bash_on_current_container.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# If a Runway container name is not provided, "CURRENT" will be used,
+# and the system will try to guess what's the most recent container.
 if [[ $# -gt 0 ]]; then
     RUNWAYCNAME="$1"
+fi
+
+if [[ $# -gt 1 ]]; then
+    LOGINUSER="$2"
+else
+    LOGINUSER="swift"
 fi
 
 # run external to the runway host
@@ -9,4 +17,8 @@ fi
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $SCRIPTDIR/lib/get_container_connection_options.sh
 
-ssh -t ${VAGRANTOPTIONS} ${RUNWAYHOST} lxc exec ${RUNWAYCNAME} -- 'sudo su - swift'
+if [[ "$LOGINUSER" != "root" ]]; then
+    ssh -t ${VAGRANTOPTIONS} ${RUNWAYHOST} lxc exec ${RUNWAYCNAME} -- sudo su - ${LOGINUSER}
+else
+    ssh -t ${VAGRANTOPTIONS} ${RUNWAYHOST} lxc exec ${RUNWAYCNAME} -- /bin/bash
+fi
