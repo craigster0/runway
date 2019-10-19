83ba9cd3a74d69ad97f576d9fff94fd12ebcfcda
Gerard Gine <ggine@swiftstack.com>
2019-10-18 17:21:48 -0700

    Force kernel.core_* settings to be loaded after reboots

--- vagrant_bootstrap.sh
+++ vagrant_bootstrap.sh
@@ -16,5 +16,13 @@ apt-get install linux-generic -y
 
 # Prepare the system to enable core dumps in the container, although core
 # dumps will not be enabled by default
-sysctl kernel.core_uses_pid=0
-sysctl kernel.core_pattern=/tmp/%e-%t-%s-%p.core
+echo 'kernel.core_uses_pid=0' >> /etc/sysctl.conf
+echo 'kernel.core_pattern=/tmp/%e-%t-%s-%p.core' >> /etc/sysctl.conf
+sysctl --system
+
+# It looks like there's been a bug in Ubuntu since (at least) 2006 that prevents
+# from properly loading the above settings, so we have to reload them at start up
+cat > /etc/cron.d/reload-sysctl << EOF
+SHELL=/bin/bash
+@reboot root /bin/sleep 5 && /sbin/sysctl --system
+EOF
